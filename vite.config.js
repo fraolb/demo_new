75d6acad71c90b53ef62f880959e42b0c7e32a3a
fraolb <Fraolbereket@gmail.com>
2023-01-04 13:42:30 +0300

    afsd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/demo_new/"
+  base:"/demo_new/",
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
